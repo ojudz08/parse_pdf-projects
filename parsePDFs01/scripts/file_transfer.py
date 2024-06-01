c36789205fb3ac2e6d93937efa0328f6ae6d26d5
Oj Rogero <ojelle.rogero@gmail.com>
2024-06-02 03:40:11 +0800

    Update parse pdf 01 (#5)
    * update
    
    * update pdf parser01

--- parsePDFs01/scripts/file_transfer.py
+++ parsePDFs01/scripts/file_transfer.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 install_dir = os.path.join(parent_dir, "dist")
@@ -6,13 +6,15 @@ install_dir = os.path.join(parent_dir, "dist")
 # transfer exe to parent directory
 if os.path.exists(install_dir):
     for exe_file in os.listdir(install_dir):
-        if exe_file[-4:] == ".exe": 
+        if exe_file[-4:] == ".exe":
             exe_src = os.path.join(install_dir, exe_file)
             exe_dst = os.path.join(parent_dir, exe_file)
+
             os.replace(exe_src, exe_dst)
 
+buildpath = os.path.join(parent_dir, "build")
+specfile = os.path.join(parent_dir, "pdf_parser.spec")
 
-# transfer spec to dist
-spec_src = os.path.join(parent_dir, "pdf_parser.spec")
-spec_dst = os.path.join(install_dir, "pdf_parser.spec")
-os.replace(spec_src, spec_dst)
+shutil.rmtree(buildpath)
+shutil.rmtree(install_dir)
+os.remove(specfile)
