9cb3eb739625b7af3c1de102ccc3ad8b366329c7
Oj Rogero <ojelle.rogero@gmail.com>
2024-06-05 19:24:19 +0800

    Add common_stocks function (#8)
    * add common_stocks func
    
    * update common_stocks func

--- parsePDFs02/main.py
+++ parsePDFs02/main.py
@@ -22,64 +22,87 @@ class pdfParse():
         self.init_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
 
     def open_file(self):
-        open_file = askopenfilenames(initialdir=self.init_dir)
+        open_file = askopenfilenames(initialdir = self.init_dir)
         filepath = open_file[0]   # read 1 file only
         return filepath
-
-
-    def read_page(self):
-        pdf_file = self.open_file()
-        read_pdf = PdfReader(pdf_file)
-        page = read_pdf.pages[0]
-        test = page.extract_text()   # resolved_objectts
-
-
-        return test
-    
-
-    def test_pymudpdf(self):
-        pdf_file = self.open_file()
-        pdf = pymupdf.open(pdf_file)
-
-        pg_cnt = pdf.page_count
         
-        cnt = 0
-        for pg in range(0, 11):
-            page = pdf.load_page(pg)
-
-            extract_pg_txt = page.get_text()
-            search_pg = page.search_for("Common Stocks")
-            if search_pg: 
-                what_page = pg + 1
-                break
-            
-            # from month_end_pages, search for Common Stocks and parse from there
-                
-        return search_pg
-    
-    def month_end_pages(self):
-        pdf_file = self.open_file()
-        pdf = pymupdf.open(pdf_file)
+    def month_end_pages(self, filepath):
+        doc = pymupdf.open(filepath)
 
         month_end = ["January 31, 2024", "February 29, 2024", "March 31, 2024"]
         month_pg = {}
-        pg_cnt = pdf.page_count     
+        pg_cnt = doc.page_count     
 
         for month in month_end:
             month_page = []
-
             for pg in range(0, pg_cnt):
-                if pdf.load_page(pg).search_for(month): month_page.append(pg)
+                if doc.load_page(pg).search_for(month): month_page.append(pg)
             
             month_pg[month] = [min(month_page), max(month_page)]
-
         return month_pg
+
+    
+    def asset_rect(self, filepath, search, pg):
+        doc = pymupdf.open(filepath)
+
+        for i in range(pg[0], pg[1] + 1):
+            page = doc.load_page(i)
+            
+            if page.search_for(search):
+                search_word = page.search_for(search)
+                search_page = i
+                break
+        return search_page, search_word
+
+
+    def common_stocks(self):
+        file_open = self.open_file()
+        filepath = file_open
+        doc = pymupdf.open(filepath)
+
+        search =  "Common Stocks"
+        pg_range = [0, 10]
+        get_page_word = self.asset_rect(filepath, search, pg_range)
+
+        pg, rect = get_page_word[0], get_page_word[1]
+
+        page = doc.load_page(pg)
+        cm_result = page.get_textbox(rect[0])
+        
+        # from the Common Stocks rect, parse the table from start to end
+        # page.rect, page.bound
+
+        return rect
+    
+
+    def bonds_and_notes(self):
+        filepath = file_open
+        doc = pymupdf.open(filepath)
+
+        search =  "Bonds and Notes"
+        pg_range = [0, 10]
+        get_page_word = self.asset_rect(filepath, search, pg_range)
+
+        pg, rect = get_page_word[0], get_page_word[1]
+        pass
+
+    def exchange_traded_funds(self):
+        pass
+
+    def mutual_funds(self):
+        pass
+
+    def affiliated_mutual_funds(self):
+        pass
+
+    def short_term_investments(self):
+        pass
       
 
 
 
 if __name__ == '__main__':
-    data = pdfParse().month_end_pages()
+    data = pdfParse().common_stocks()
 
     print(data)
     
