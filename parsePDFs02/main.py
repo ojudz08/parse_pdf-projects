4060ae1ba8eda1172b59e6e9eb03082073068b41
Oj Rogero <ojelle.rogero@gmail.com>
2024-06-09 07:29:42 +0800

    Update parsepdf 02 project (#9)
    * common stocks func
    
    * bonds and notes func
    
    * add other functions

--- parsePDFs02/main.py
+++ parsePDFs02/main.py
@@ -20,114 +20,241 @@ import pandas as pd
 
 class pdfParse():
 
-    def __init__(self) -> None:
-        self.init_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+    def __init__(self, filepath) -> None:
+        self.filepath = filepath
 
-    def open_file(self):
-        open_file = askopenfilenames(initialdir = self.init_dir)
-        filepath = open_file[0]   # read 1 file only
-        return filepath
-        
-    def month_end_pages(self, filepath):
-        doc = pymupdf.open(filepath)
-
-        month_end = ["January 31, 2024", "February 29, 2024", "March 31, 2024"]
-        month_pg = {}
+    def month_end_pages(self, period):
+        doc = pymupdf.open(self.filepath)
         pg_cnt = doc.page_count     
 
-        for month in month_end:
-            month_page = []
-            for pg in range(0, pg_cnt):
-                if doc.load_page(pg).search_for(month): month_page.append(pg)
+        month_page = []
+        for pg in range(0, pg_cnt):
+            if doc.load_page(pg).search_for(period): month_page.append(pg)
             
-            month_pg[month] = [min(month_page), max(month_page)]
-        return month_pg
+            pg_range = [min(month_page), max(month_page)]            
+        return pg_range
 
     
-    def asset_rect(self, filepath, search, pg):
-        doc = pymupdf.open(filepath)
+    def asset_rect(self, search, pgrng):
+        doc = pymupdf.open(self.filepath)
 
-        for i in range(pg[0], pg[1] + 1):
+        for i in range(pgrng[0], pgrng[1] + 1):
             page = doc.load_page(i)
             
             if page.search_for(search):
-                search_word = page.search_for(search)
+                search_word = page.search_for(search)[0]
                 search_page = i
                 break
+                
         return search_page, search_word
 
 
-    def read_area(self, filepath, pg, rectarea):
-        df = tabula.read_pdf(filepath, pages=pg, area=[rectarea], pandas_options={'header': None}, output_format='dataframe', stream=True)[0]
+    def read_area(self, pg, rectarea):
+        df = tabula.read_pdf(self.filepath, pages=pg, area=[rectarea], pandas_options={'header': None}, output_format='dataframe', stream=True)[0]
+        return df
+    
+
+    def asset_pg(self, period_pg):
+        assets = ["Common Stocks", "Bonds and Notes", "Exchange-Traded Funds", "Mutual Funds", "Affiliated Mutual Funds", "Short-Term Investments"]
+
+        assets_pg = {}
+        for i in range(0, len(assets)):
+            asset_i = self.asset_rect(assets[i], period_pg)
+            
+            if i <= 3:
+                asset_next = self.asset_rect(assets[i + 1], period_pg)
+                pg_i, pg_nxt = asset_i[0], asset_next[0]
+                rect_i, rect_next = round(asset_i[1][1], 2), round(asset_next[1][1], 2)
+                
+                if pg_nxt > pg_i and rect_next > rect_i:
+                    assets_pg[assets[i]] = [asset_i[0], asset_next[0]]
+                elif pg_nxt > pg_i and rect_next < 188:
+                    assets_pg[assets[i]] = [asset_i[0], asset_i[0]]
+                elif pg_nxt == pg_i:
+                    assets_pg[assets[i]] = [asset_i[0], asset_i[0]]
+            else:
+                assets_pg[assets[i]] = [asset_i[0], asset_i[0]]
+            
+        return assets_pg
+    
+    
+    def concat_cols(self, rect_area, pg):
+        data, colrename = pd.DataFrame(), []
+        for key, value in rect_area.items():
+            df = self.read_area(pg, value)
+            data = pd.concat([data, df], axis=1, ignore_index=True)
+            colrename.append(key)
+        
+        return data, colrename
+
+
+    def get_rect_area(self, asset, top, bot):
+
+        if asset in ('Common Stocks', 'Exchange-Traded Funds', 'Mutual Funds', 'Affiliated Mutual Funds'):
+            rect_area = {"Shares": [top, 60, bot, 91],
+                         "Security Description": [top, 94, bot, 330],
+                         "Market Value ($)": [top, 425, bot, 487],
+                         f"% of Fund": [top, 490, bot, 540]}
+
+        elif asset in ('Bonds and Notes', 'Short-Term Investments'):
+            rect_area = {"Principal Amount": [top, 60, bot, 91],
+                         "Security Description": [top, 94, bot, 330],
+                         "Interest Rate": [top, 332, bot, 370],
+                         "Maturity Date": [top, 373, bot, 420],
+                         "Market Value ($)": [top, 425, bot, 487],
+                         f"% of Fund": [top, 490, bot, 540]}
+
+        return rect_area
+    
+
+    def common_stocks(self, asset, page_range):
+        pgrng = page_range
+
+        df = pd.DataFrame()
+        strt, end = pgrng[0] + 1, pgrng[1] + 2
+
+        for pg in range(strt, end):
+            if pg == 1: top, bot = 130, 725            
+            elif pg > 1 and pg < end - 1: top, bot = 117, 725
+            elif pg == end - 1: top, bot = 117, 220
+
+            rect_area = self.get_rect_area(asset, top, bot)
+
+            temp = self.concat_cols(rect_area, pg)
+            data, colrename = temp[0], temp[1]
+            df = pd.concat([df, data], axis=0, ignore_index=True)
+
+        df.columns = colrename
+        return df
+    
+
+    def bonds_and_notes(self, asset, page_range):
+        pgrng = page_range
+
+        df = pd.DataFrame()
+        strt, end = pgrng[0] + 1, pgrng[1] + 2
+
+        for pg in range(strt, end):
+            if pg == strt: top, bot = 250, 725       # add a function to derive top 250     
+            elif pg > strt and pg < end - 1: top, bot = 117, 725
+            elif pg == end - 1: top, bot = 117, 630  # add a function to derive bot 
+            
+            rect_area = self.get_rect_area(asset, top, bot)
+
+            temp = self.concat_cols(rect_area, pg)
+            data, colrename = temp[0], temp[1]
+            df = pd.concat([df, data], axis=0, ignore_index=True)
+
+        df.columns = colrename
         return df
 
 
-    def common_stocks(self):
-        file_open = self.open_file()
-        filepath = file_open
-        doc = pymupdf.open(filepath)
+    def exchange_traded_funds(self, asset, page_range):
+        pgrng = page_range
 
-        search =  "Common Stocks"
-        pgrng = [0, 10]
-        get_page_word = self.asset_rect(filepath, search, pgrng)
+        df = pd.DataFrame()
+        strt, end = pgrng[0] + 1, pgrng[1] + 2
 
-        pag, rect = get_page_word[0], get_page_word[1]
+        for pg in range(strt, end):
+            if pg == strt: top, bot = 660, 725
 
-        page = doc.load_page(pag)
-        cm_result = page.get_textbox(rect[0])
-        
-        common_stocks_df = pd.DataFrame()
-        strt, end = pgrng[0] + 1, 9
-        for pg in range(strt, 9):
-            if pg == 1: top, bot = 130, 725            
-            elif pg > 1 and pg < 8: top, bot = 117, 725
-            elif pg == 8: top, bot = 117, 220
+            rect_area = self.get_rect_area(asset, top, bot)
+            
+            temp = self.concat_cols(rect_area, pg)
+            data, colrename = temp[0], temp[1]
+            df = pd.concat([df, data], axis=0, ignore_index=True)
 
-            rect_area = {"Shares": [top, 60, bot, 91], "Security Description": [top, 94, bot, 400], "Market Value ($)": [top, 420, bot, 487], f"% of Fund": [top, 490, bot, 540]}
+        df.columns = colrename
+        return df
 
-            data, colrename = pd.DataFrame(), []
-            for key, value in rect_area.items():
-                df = self.read_area(filepath, pg, value)
-                data = pd.concat([data, df], axis=1, ignore_index=True)
-                colrename.append(key)
-        
-            common_stocks_df = pd.concat([common_stocks_df, data], axis=0, ignore_index=True)
 
-        common_stocks_df.columns = colrename
+    def mutual_funds(self, asset, page_range):
+        pgrng = page_range
 
-        return common_stocks_df
+        df = pd.DataFrame()
+        strt, end = pgrng[0] + 1, pgrng[1] + 2
+
+        for pg in range(strt, end):
+            if pg == strt: top, bot = 134, 185   # add function to derive both
+
+            rect_area = self.get_rect_area(asset, top, bot)
+            
+            temp = self.concat_cols(rect_area, pg)
+            data, colrename = temp[0], temp[1]
+            df = pd.concat([df, data], axis=0, ignore_index=True)
+
+        df.columns = colrename
+        return df
     
 
-    def bonds_and_notes(self):
-        file_open = self.open_file()
-        filepath = file_open
-        doc = pymupdf.open(filepath)
+    def affiliated_mutual_funds(self, asset, page_range):
+        pgrng = page_range
 
-        search =  "Bonds and Notes"
-        pg_range = [0, 10]
-        get_page_word = self.asset_rect(filepath, search, pg_range)
+        df = pd.DataFrame()
+        strt, end = pgrng[0] + 1, pgrng[1] + 2
 
-        pg, rect = get_page_word[0], get_page_word[1]
+        for pg in range(strt, end):
+            if pg == strt: top, bot = 199, 300   # add function to derive both
 
-        return pg, rect
+            rect_area = self.get_rect_area(asset, top, bot)
+            
+            temp = self.concat_cols(rect_area, pg)
+            data, colrename = temp[0], temp[1]
+            df = pd.concat([df, data], axis=0, ignore_index=True)
 
-    def exchange_traded_funds(self):
-        pass
+        df.columns = colrename
+        return df
+    
 
-    def mutual_funds(self):
-        pass
+    def short_term_investments(self, asset, page_range):
+        pgrng = page_range
 
-    def affiliated_mutual_funds(self):
-        pass
+        df = pd.DataFrame()
+        strt, end = pgrng[0] + 1, pgrng[1] + 2
 
-    def short_term_investments(self):
-        pass
-      
+        for pg in range(strt, end):
+            if pg == strt: top, bot = 322, 390   # add function to derive both
+
+            rect_area = self.get_rect_area(asset, top, bot)
+            
+            temp = self.concat_cols(rect_area, pg)
+            data, colrename = temp[0], temp[1]
+            df = pd.concat([df, data], axis=0, ignore_index=True)
+
+        df.columns = colrename
+        return df
+    
+
+    def get_data(self):
+        period = "March 31, 2024"
+        period_pg = self.month_end_pages(period)
+
+        asset_pages = self.asset_pg(period_pg)
+        for key, val in asset_pages.items():
+            if key == "Common Stocks":
+                asset1 = self.common_stocks(key, val)
+            elif key == "Bonds and Notes":
+                asset2 = self.bonds_and_notes(key, val)
+            elif key == "Exchange-Traded Funds":
+                asset3 = self.exchange_traded_funds(key, val)
+            elif key == "Mutual Funds":
+                asset4 = self.mutual_funds(key, val)
+            elif key == "Affiliated Mutual Funds":
+                asset5 = self.affiliated_mutual_funds(key, val)
+            elif key == "Short-Term Investments":
+                asset6 = self.short_term_investments(key, val)
+
+        return asset6
+
 
 
 
 if __name__ == '__main__':
-    data = pdfParse().common_stocks()
-
+    #month_end = ["January 31, 2024", "February 29, 2024", "March 31, 2024"]
+    init_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+    open_file = askopenfilenames(initialdir = init_dir)
+    filepath = open_file[0]    
+     
+    data = pdfParse(filepath).get_data()
     print(data)
     
