08da277f13758ff2499c336bb704a2763bfe0508
Ojelle Rogero <ojelle.rogero@gmail.com>
2024-06-09 16:59:09 +0800

    upload pdf reports

---
+++ splitPDFs/main.py
@@ -0,0 +1,12 @@
+"""
+    Author: Ojelle Rogero
+    Created on: June 09, 2024
+    About:
+        Split the pdf
+"""
+
+import os, sys
+from tkinter.filedialog import *
+import tabula
+import pymupdf
+import pandas as pd
