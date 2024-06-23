6ce5aa03b7fd38281d9353d64f7b075f2908a0c7
Ojelle Rogero <ojelle.rogero@gmail.com>
2024-06-23 22:47:51 +0800

    update main.py

--- parsePDFs03/main.py
+++ parsePDFs03/main.py
@@ -5,14 +5,31 @@
         Parses the transactions of a bank statement
 """
 
-import os, sys
-from tkinter.filedialog import *
-import tabula
-import pymupdf
+import pymupdf, tabula
+import os, sys, time
+from config import config_cred
 import pandas as pd
+from openpyxl import load_workbook
 
 
 
+class bank_estatement():
+
+    def __init__(self):
+        self.init_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+
+    def bound_box(self, doc_page):
+        try:
+            top = doc_page.search_for("Sale Date")[0].irect[3]
+            left = doc_page.search_for("Sale Date")[0].irect[0]
+            bot = doc_page.search_for("For more details")[0].irect[3] - 4
+            right = doc_page.search_for("Amount")[0].irect[2]
+        
+            rect_area = [top, left, bot, right]
+            return rect_area
+        except:
+            return None
 
 
 
