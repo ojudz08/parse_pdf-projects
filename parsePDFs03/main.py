eb8261f5761947e7b7fefe3010fd489ce246f618
Ojelle Rogero <ojelle.rogero@gmail.com>
2024-06-23 22:52:18 +0800

    update

--- parsePDFs03/main.py
+++ parsePDFs03/main.py
@@ -30,6 +30,34 @@ class bank_estatement():
             return rect_area
         except:
             return None
+    
+
+    def bdo_es(self, es_dir):
+        for filename in os.listdir(es_dir):
+            file_path = os.path.join(es_dir, filename)
+            doc = pymupdf.open(file_path)
+            pg_cnt = doc.page_count
+            
+            if doc.authenticate(config_cred()[1]) != 6: break
+            
+            result = pd.DataFrame()
+            for pg in range(0, pg_cnt):
+                rect_area = bound_box(doc.load_page(pg))
+                
+                if rect_area == None: continue
+
+                df = tabula.read_pdf(file_path,
+                                    password = config_cred()[1],
+                                    pages = pg + 1,
+                                    area = rect_area,
+                                    pandas_options = {'header': None},
+                                    output_format = 'dataframe',
+                                    stream = True)[0]
+                result = pd.concat([result, df], axis=0, ignore_index=True)
+            
+            result = result.rename(columns={0: "Sale Date", 1: "Post Date", 2: "Transaction Details", 3: "Amount"}).drop(index=[0, 1]).reset_index()
+            result = result.drop('index', axis=1)
+            return result
 
 
 
